cd380b9b1e04c07026c2e51362e63278fbecad62
Rasmus Eriksson <rer1117@student.skelleftea.se>
2026-08-27 14:47:46 +0200

    mobile bugfix?

--- periodiskasystemet/script.js
+++ periodiskasystemet/script.js
@@ -4,19 +4,15 @@ let random = 0
 let index = 1
 let random2 = 0
 let n = 1
-let t = 10
 let text = document.getElementById("text");
 document.addEventListener("mousedown", mouseDown);
 function mouseDown() {
     runTest()
 }
-document.addEventListener("touchstart", touchStart)
-function touchStart() {
-    t -= 1
-    if (t <= 0) {
-        runTest()
-        t = 10
-    }
+document.addEventListener("touchend", touchEnd)
+document.addEventListener("touchcancel", touchEnd)
+function touchEnd() {
+    runTest()
 }
 function runTest() {
     n *= -1
