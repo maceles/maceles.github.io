cd74e592c7c462e63c43a27ee5f57f1ccefc4d25
Rasmus Eriksson <rer1117@student.skelleftea.se>
2026-08-27 14:37:13 +0200

    bugfixes

--- periodiskasystemet/script.js
+++ periodiskasystemet/script.js
@@ -22,20 +22,20 @@ function runTest() {
         random = Math.round(Math.random() * (short.length - 1));
         random2 = Math.round(Math.random() * 3)
         text.textContent = ""
-        if (random2 == 0, random2 == 3) {
+        if (random2 == 0 || random2 == 3) {
             text.append(short[random])
         } else if (random2 == 1) {
             text.append(names[random])
         } else {
-            text.append(random + 1)
+            text.append((random + 1))
         }
     } else {
-        if (random2 == 0, random2 == 3) {
+        if (random2 == 0 || random2 == 3) {
             text.append(" " + names[random])
-            text.append(" " + random + 1)
+            text.append(" " + (random + 1))
         } else if (random2 == 1) {
             text.append(" " + short[random])
-            text.append(" " + random + 1)
+            text.append(" " + (random + 1))
         } else {
             text.append(" " + names[random])
             text.append(" " + short[random])
